30b019fe6f1eacedc2d41550496a959693f9cf60
LeanCompiler <leancompiler@gmail.com>
2023-10-25 10:43:35 +0530

    add inital build

---
+++ hook/useFetch.js
@@ -0,0 +1,50 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+//import {RAPID_API_KEY} from "@env";
+/* import dotenv from "dotenv";
+dotenv.config(); */
+
+const rapidApiKey = `ce219c1a10msha8a13aa53a3471bp1e3889jsne9ca19d0aa76`;
+
+export const useFetch = (endpoint, query) => {
+    const [data, setData] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState(null);
+
+    const options = {
+        method: 'GET',
+        url: `https://jsearch.p.rapidapi.com/${endpoint}`,
+        headers: {
+            'X-RapidAPI-Key': rapidApiKey,
+            'X-RapidAPI-Host': 'jsearch.p.rapidapi.com'
+        },
+        params: { ...query }
+    };
+
+    const fetchData = async () => {
+        setIsLoading(true);
+
+        try {
+            const response = await axios.request(options);
+
+            setData(response.data.data);
+            setIsLoading(false);
+        } catch (error) {
+            setError(error);
+            alert('There is an error')
+        } finally {
+            setIsLoading(false);
+        }
+    };
+
+    useEffect(() => {
+        fetchData();
+    }, []);
+
+    const refetch = () => {
+        setIsLoading(true);
+        fetchData();
+    }
+
+    return { data, isLoading, error, refetch };
+};
